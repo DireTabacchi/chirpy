698340cb8c756c923dca707b8837a67a2af5a15a
Nathaniel Tabacchi <ntabacchi@outlook.com>
2024-07-08 13:31:40 -0400

    Server serves basic html and image, and has a readiness endpoint.

--- main.go
+++ main.go
@@ -7,14 +7,33 @@ import (
 
 func main() {
     const port = "8080"
+    const filepathRoot = "."
+    const filepathLogo = "./assets/logo.png"
 
     serveMux := http.NewServeMux()
+    serveMux.Handle("/app/*",
+        http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))),
+    )
+    serveMux.Handle("/app/assets/",
+        http.StripPrefix("/app/assets/", http.FileServer(http.Dir("assets"))),
+    )
+    serveMux.HandleFunc("/healthz", readinessHandler)
 
     srv := &http.Server{
         Addr: ":" + port,
         Handler: serveMux,
     }
 
-    log.Printf("Serving on port: %s\n", port)
+    log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
     log.Fatal(srv.ListenAndServe())
+
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(200)
+    _, err := w.Write([]byte("OK"))
+    if err != nil {
+        log.Printf("Readiness handler exprienced error while writing body:\n%v", err)
+    }
 }
